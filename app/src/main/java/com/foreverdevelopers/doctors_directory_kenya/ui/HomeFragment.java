d7ca81c146083c3e82d243c5badfdd7d66826ca8
KennBuluma <kennbuluma@outlook.com>
2021-10-04 17:03:47 +0300

    Sync the unsynchrony my dude

--- app/src/main/java/com/foreverdevelopers/doctors_directory_kenya/ui/HomeFragment.java
+++ app/src/main/java/com/foreverdevelopers/doctors_directory_kenya/ui/HomeFragment.java
@@ -43,13 +43,16 @@ import com.foreverdevelopers.doctors_directory_kenya.data.viewmodel.DoctorViewMo
 import com.foreverdevelopers.doctors_directory_kenya.data.viewmodel.FacilityViewModel;
 import com.foreverdevelopers.doctors_directory_kenya.data.viewmodel.ServiceViewModel;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class HomeFragment extends Fragment {
 
     private AppViewModel appViewModel;
-    private NavController appNavController;
     private CountyViewModel countyViewModel;
     private FacilityViewModel facilityViewModel;
     private ServiceViewModel serviceViewModel;
@@ -57,6 +60,10 @@ public class HomeFragment extends Fragment {
     private ArrayAdapter<Doctor> doctorAdapter;
     private AutoCompleteTextView atvSearch;
     private HashMap<Integer, PathData> pathDataHashMap;
+    private List<County> counties = new ArrayList<>();
+    private List<Facility> facilities = new ArrayList<>();
+    private List<Service> services = new ArrayList<>();
+    private List<Doctor> doctors = new ArrayList<>();
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
@@ -70,12 +77,6 @@ public class HomeFragment extends Fragment {
         facilityViewModel = new ViewModelProvider(requireActivity()).get(FacilityViewModel.class);
         serviceViewModel = new ViewModelProvider(requireActivity()).get(ServiceViewModel.class);
         doctorViewModel = new ViewModelProvider(requireActivity()).get(DoctorViewModel.class);
-        appViewModel.navController.observe(getViewLifecycleOwner(), new Observer<NavController>() {
-            @Override
-            public void onChanged(NavController navController) {
-                appNavController = navController;
-            }
-        });
         final View root = inflater.inflate(R.layout.fragment_home, container, false);
         loadComponents(root);
         return root;
@@ -87,69 +88,85 @@ public class HomeFragment extends Fragment {
             btnServices = root.findViewById(R.id.btn_main_services),
             btnFacilities = root.findViewById(R.id.btn_main_facilities);
         atvSearch = root.findViewById(R.id.atv_main_search);
+        btnCounties.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pathDataHashMap = new HashMap<Integer, PathData>();
+                pathDataHashMap.put(0, new PathData(null,null,R.id.nav_home));
+                pathDataHashMap.put(1, new PathData(RA_COUNTIES,null,R.id.nav_counties));
+                pathDataHashMap.put(2, new PathData(RA_FACILITIES_BY_COUNTY,null,R.id.nav_facilities));
+                pathDataHashMap.put(3, new PathData(RA_SERVICES_BY_FACILITY,null,R.id.nav_services));
+                pathDataHashMap.put(4, new PathData(RA_DOCTORS_BY_SERVICE,null,R.id.nav_doctors));
+                pathDataHashMap.put(5, new PathData(RA_DOCTOR_DETAILS,null,R.id.nav_doctor_details));
+                countyViewModel.setFilteredCounties(counties);
+                appViewModel.setCurrentPathMap(pathDataHashMap);
+                appViewModel.setCurrentIndexor(new Indexor(1,null));
+            }
+        });
+        btnFacilities.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pathDataHashMap = new HashMap<Integer, PathData>();
+                pathDataHashMap.put(0, new PathData(null,null,R.id.nav_home));
+                pathDataHashMap.put(1, new PathData(RA_FACILITIES,null,R.id.nav_facilities));
+                pathDataHashMap.put(2, new PathData(RA_COUNTIES_BY_FACILITY,null,R.id.nav_counties));
+                pathDataHashMap.put(3, new PathData(RA_SERVICES_BY_COUNTY,null,R.id.nav_services));
+                pathDataHashMap.put(4, new PathData(RA_DOCTORS_BY_SERVICE,null,R.id.nav_doctors));
+                pathDataHashMap.put(5, new PathData(RA_DOCTOR_DETAILS,null,R.id.nav_doctor_details));
+                facilityViewModel.setFilteredFacilities(facilities);
+                appViewModel.setCurrentPathMap(pathDataHashMap);
+                appViewModel.setCurrentIndexor(new Indexor(1,null));
+            }
+        });
+        btnServices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pathDataHashMap = new HashMap<Integer, PathData>();
+                pathDataHashMap.put(0, new PathData(null,null,R.id.nav_home));
+                pathDataHashMap.put(1, new PathData(RA_SERVICES,null,R.id.nav_services));
+                pathDataHashMap.put(2, new PathData(RA_COUNTIES_BY_SERVICE,null,R.id.nav_counties));
+                pathDataHashMap.put(3, new PathData(RA_FACILITIES_BY_COUNTY,null,R.id.nav_facilities));
+                pathDataHashMap.put(4, new PathData(RA_DOCTORS_BY_FACILITY,null,R.id.nav_doctors));
+                pathDataHashMap.put(5, new PathData(RA_DOCTOR_DETAILS,null,R.id.nav_doctor_details));
+                serviceViewModel.setFilteredServices(services);
+                appViewModel.setCurrentPathMap(pathDataHashMap);
+                appViewModel.setCurrentIndexor(new Indexor(1,null));
+            }
+        });
+        btnDoctors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pathDataHashMap = new HashMap<Integer, PathData>();
+                pathDataHashMap.put(0, new PathData(null,null,R.id.nav_home));
+                pathDataHashMap.put(1, new PathData(RA_DOCTORS,null,R.id.nav_doctors));
+                pathDataHashMap.put(2, new PathData(RA_DOCTOR_DETAILS,null,R.id.nav_doctor_details));
+                doctorViewModel.setFilteredDoctors(doctors);
+                appViewModel.setCurrentPathMap(pathDataHashMap);
+                appViewModel.setCurrentIndexor(new Indexor(1,null));
+            }
+        });
         countyViewModel.counties.observe(getViewLifecycleOwner(), new Observer<List<County>>() {
             @Override
-            public void onChanged(List<County> counties) {
-                btnCounties.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        pathDataHashMap = new HashMap<Integer, PathData>();
-                        pathDataHashMap.put(0, new PathData(null,null,R.id.nav_home));
-                        pathDataHashMap.put(1, new PathData(RA_COUNTIES,null,R.id.nav_counties));
-                        pathDataHashMap.put(2, new PathData(RA_FACILITIES_BY_COUNTY,null,R.id.nav_facilities));
-                        pathDataHashMap.put(3, new PathData(RA_SERVICES_BY_FACILITY,null,R.id.nav_services));
-                        pathDataHashMap.put(4, new PathData(RA_DOCTORS_BY_SERVICE,null,R.id.nav_doctors));
-                        pathDataHashMap.put(5, new PathData(RA_DOCTOR_DETAILS,null,R.id.nav_doctor_details));
-                        countyViewModel.setFilteredCounties(counties);
-                        appViewModel.setCurrentPathMap(pathDataHashMap);
-                        appViewModel.setCurrentIndexor(new Indexor(1,null));
-                    }
-                });
+            public void onChanged(List<County> dbCounties) {
+                counties = dbCounties;
             }
         });
         facilityViewModel.facilities.observe(getViewLifecycleOwner(), new Observer<List<Facility>>() {
             @Override
-            public void onChanged(List<Facility> facilities) {
-                btnFacilities.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        pathDataHashMap = new HashMap<Integer, PathData>();
-                        pathDataHashMap.put(0, new PathData(null,null,R.id.nav_home));
-                        pathDataHashMap.put(1, new PathData(RA_FACILITIES,null,R.id.nav_facilities));
-                        pathDataHashMap.put(2, new PathData(RA_COUNTIES_BY_FACILITY,null,R.id.nav_counties));
-                        pathDataHashMap.put(3, new PathData(RA_SERVICES_BY_COUNTY,null,R.id.nav_services));
-                        pathDataHashMap.put(4, new PathData(RA_DOCTORS_BY_SERVICE,null,R.id.nav_doctors));
-                        pathDataHashMap.put(5, new PathData(RA_DOCTOR_DETAILS,null,R.id.nav_doctor_details));
-                        facilityViewModel.setFilteredFacilities(facilities);
-                        appViewModel.setCurrentPathMap(pathDataHashMap);
-                        appViewModel.setCurrentIndexor(new Indexor(1,null));
-                    }
-                });
+            public void onChanged(List<Facility> dbFacilities) {
+                facilities = dbFacilities;
             }
         });
         serviceViewModel.services.observe(getViewLifecycleOwner(), new Observer<List<Service>>() {
             @Override
-            public void onChanged(List<Service> services) {
-                btnServices.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        pathDataHashMap = new HashMap<Integer, PathData>();
-                        pathDataHashMap.put(0, new PathData(null,null,R.id.nav_home));
-                        pathDataHashMap.put(1, new PathData(RA_SERVICES,null,R.id.nav_services));
-                        pathDataHashMap.put(2, new PathData(RA_COUNTIES_BY_SERVICE,null,R.id.nav_counties));
-                        pathDataHashMap.put(3, new PathData(RA_FACILITIES_BY_COUNTY,null,R.id.nav_facilities));
-                        pathDataHashMap.put(4, new PathData(RA_DOCTORS_BY_FACILITY,null,R.id.nav_doctors));
-                        pathDataHashMap.put(5, new PathData(RA_DOCTOR_DETAILS,null,R.id.nav_doctor_details));
-                        serviceViewModel.setFilteredServices(services);
-                        appViewModel.setCurrentPathMap(pathDataHashMap);
-                        appViewModel.setCurrentIndexor(new Indexor(1,null));
-                    }
-                });
+            public void onChanged(List<Service> dbServices) {
+                services = dbServices;
             }
         });
         doctorViewModel.doctors.observe(getViewLifecycleOwner(), new Observer<List<Doctor>>() {
             @Override
-            public void onChanged(List<Doctor> doctors) {
+            public void onChanged(List<Doctor> dbDoctors) {
+                doctors = dbDoctors;
                 doctorAdapter = new DoctorsArrayAdapter(requireContext(), R.layout.list_item_name, doctors);
                 atvSearch.setAdapter(doctorAdapter);
                 atvSearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -159,18 +176,6 @@ public class HomeFragment extends Fragment {
                         pathDataHashMap = new HashMap<Integer, PathData>();
                         pathDataHashMap.put(0, new PathData(null,null,R.id.nav_home));
                         pathDataHashMap.put(1, new PathData(RA_DOCTOR_DETAILS,doctor,R.id.nav_doctor_details));
-                        doctorViewModel.setFilteredDoctors(doctors);
-                        appViewModel.setCurrentPathMap(pathDataHashMap);
-                        appViewModel.setCurrentIndexor(new Indexor(1,null));
-                    }
-                });
-                btnDoctors.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        pathDataHashMap = new HashMap<Integer, PathData>();
-                        pathDataHashMap.put(0, new PathData(null,null,R.id.nav_home));
-                        pathDataHashMap.put(1, new PathData(RA_DOCTORS,null,R.id.nav_doctors));
-                        pathDataHashMap.put(2, new PathData(RA_DOCTOR_DETAILS,null,R.id.nav_doctor_details));
                         doctorViewModel.setFilteredDoctors(doctors);
                         appViewModel.setCurrentPathMap(pathDataHashMap);
                         appViewModel.setCurrentIndexor(new Indexor(1,null));
